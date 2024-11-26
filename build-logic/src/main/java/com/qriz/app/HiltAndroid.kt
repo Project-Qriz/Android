c15ab7ae3f716746a7a8f879fb4d20a840cec7bb
KimKwangYoung <kwangyoung.dev@gmail.com>
2024-11-26 20:26:49 +0900

    chore : build-logic 추가

---
+++ build-logic/src/main/java/com/qriz/app/HiltAndroid.kt
@@ -0,0 +1,16 @@
+package com.qriz.app
+
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+fun Project.configureHiltAndroid() {
+    with(pluginManager) {
+        apply("dagger.hilt.android.plugin")
+        apply("com.google.devtools.ksp")
+    }
+
+    dependencies {
+        "implementation"(findLibrary("hilt"))
+        "ksp"(findLibrary("hiltCompiler"))
+    }
+}
---
+++ build-logic/src/main/java/qriz.android.feature.gradle.kts
@@ -0,0 +1,7 @@
+import com.qriz.app.configureComposeAndroid
+
+plugins {
+    id("qriz.android.library")
+}
+
+configureComposeAndroid()
