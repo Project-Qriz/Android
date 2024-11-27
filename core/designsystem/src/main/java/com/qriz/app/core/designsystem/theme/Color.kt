6b292ec96dcfb8680fff1b163601ad5d0b672d6b
KimKwangYoung <kwangyoung.dev@gmail.com>
2024-11-27 21:12:29 +0900

    ui : Theme 설정
    - color
    - window view color

--- core/designsystem/src/main/java/com/qriz/app/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/qriz/app/core/designsystem/theme/Color.kt
@@ -2,10 +2,33 @@ package com.qriz.app.core.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Gray100 = Color(0xFFF0F4F7)
+val Gray200 = Color(0xFFCDD5E0)
+val Gray300 = Color(0xFFAAB6C9)
+val Gray400 = Color(0xFF8F9AAA)
+val Gray500 = Color(0xFF747D8B)
+val Gray600 = Color(0xFF5A616B)
+val Gray700 = Color(0xFF3F444C)
+val Gray800 = Color(0xFF24282D)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Blue100 = Color(0xFFF8F9FD)
+val Blue200 = Color(0xFFEEF0F8)
+val Blue300 = Color(0xFFD5DFFF)
+val Blue400 = Color(0xFF93AEFE)
+val Blue500 = Color(0xFF668EFE)
+val Blue600 = Color(0xFF3A6EFE) //primary
+val Blue700 = Color(0xFF265FFF)
+val Blue800 = Color(0xFF265FFF)
+val Blue900 = Color(0xFF1A4BD1)
+
+val Mint100 = Color(0xFFD2FFEC)
+val Mint200 = Color(0xFFB0F2DB)
+val Mint300 = Color(0xFF99E8D0)
+val Mint400 = Color(0xFF81DEC4)
+val Mint500 = Color(0xFF6AD4B9)
+val Mint600 = Color(0xFF52CAAE) //tertiary
+
+val Red500 = Color(0xFFEF5D5D)
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
--- core/designsystem/src/main/java/com/qriz/app/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/qriz/app/core/designsystem/theme/Theme.kt
@@ -1,55 +1,53 @@
 package com.qriz.app.core.designsystem.theme
 
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
+import android.app.Activity
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val QrizColorScheme = lightColorScheme(
+    primary = Blue600,
+    onPrimary = White,
+    primaryContainer = Blue200,
+    onPrimaryContainer = Blue500,
+    secondaryContainer = Gray100,
+    tertiary = Mint600,
+    tertiaryContainer = Mint100,
+    surface = White,
+    onSurface = Gray800,
+    surfaceVariant = Gray600,
+    onSurfaceVariant = Gray500,
+    outline = Blue600,
+    background = White,
+    error = Red500,
 )
 
 @Composable
 fun QrizTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true, content: @Composable () -> Unit
+    darkTheme: Boolean = false,
+    content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val colorScheme = QrizColorScheme
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    if (!LocalInspectionMode.current) {
+        val view = LocalView.current
+        val activity = view.context as Activity
+        val window = activity.window
+        WindowCompat.getInsetsController(window, activity.window.decorView).apply {
+            isAppearanceLightStatusBars = darkTheme.not()
+            isAppearanceLightNavigationBars = darkTheme.not()
+        }
+        window.statusBarColor = colorScheme.background.toArgb()
+        window.navigationBarColor = colorScheme.background.toArgb()
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = QrizColorScheme,
         typography = Typography,
         content = content
     )
