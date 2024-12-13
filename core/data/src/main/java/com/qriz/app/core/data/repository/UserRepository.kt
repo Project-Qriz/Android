e13c5d693176dfea2c0c5197da67e3f9980b3484
KimKwangYoung <kwangyoung.dev@gmail.com>
2024-12-13 23:54:01 +0900

    feat : User Data 레이어 부분 형태 잡기

--- core/data/src/main/java/com/qriz/app/core/data/repository/UserRepository.kt
+++ core/data/src/main/java/com/qriz/app/core/data/repository/UserRepository.kt
@@ -1,7 +1,26 @@
 package com.qriz.app.core.data.repository
 
+import com.qriz.app.core.data.model.user.User
 import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
     val flowLogin: Flow<Boolean>
+
+    suspend fun login(
+        id: String,
+        password: String,
+    ): User
+
+    suspend fun sendAuthenticationNumber(email: String): Boolean
+
+    suspend fun verifyAuthenticationNumber(authenticationNumber: String): Boolean
+
+    suspend fun checkDuplicateId(id: String): Boolean
+
+    suspend fun signUp(
+        loginId: String,
+        password: String,
+        email: String,
+        nickname: String,
+    ): User
 }
