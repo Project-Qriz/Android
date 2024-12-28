e70abda5fdc35188e133a2cfee9244813f097a7d
Tom <blak1002@gmail.com>
2024-12-28 17:18:48 +0900

    [config] Kotlin 컴파일 시 JVM 타겟 17로 설정

--- build-logic/src/main/java/com/qriz/app/KotlinAndroid.kt
+++ build-logic/src/main/java/com/qriz/app/KotlinAndroid.kt
@@ -2,6 +2,9 @@ package com.qriz.app
 
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun Project.configureKotlinAndroid() {
     pluginManager.apply("org.jetbrains.kotlin.android")
@@ -26,6 +29,15 @@ fun Project.configureKotlinAndroid() {
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_17
             targetCompatibility = JavaVersion.VERSION_17
+        }
+    }
+    configureKotlin()
+}
+
+private fun Project.configureKotlin() {
+    tasks.withType<KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
 }
