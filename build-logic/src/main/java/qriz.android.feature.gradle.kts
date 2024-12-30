cbc09a8ec957298a7c91d64644956d8e25713890
Tom <blak1002@gmail.com>
2024-12-30 20:42:47 +0900

    [refactor/#12] qriz.android.library.testable.gradle 플러그인 생성으로 테스트 관련 의존성 반복 명시 해결

--- build-logic/src/main/java/qriz.android.feature.gradle.kts
+++ build-logic/src/main/java/qriz.android.feature.gradle.kts
@@ -1,14 +1,13 @@
 import com.qriz.app.findLibrary
 
 plugins {
-    id("qriz.android.library")
+    id("qriz.android.library.testable")
     id("qriz.android.compose")
 }
 
 dependencies {
     implementation(project(":core:designsystem"))
     implementation(project(":core:navigation"))
-    testImplementation(project(":core:testing"))
 
     implementation(findLibrary("androidx.core.ktx"))
     implementation(findLibrary("androidx.appcompat"))
