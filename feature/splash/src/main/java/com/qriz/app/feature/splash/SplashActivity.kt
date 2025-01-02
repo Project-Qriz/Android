1a66cee0cb48754ff8ce7d977ca481938cb156ae
Tom <blak1002@gmail.com>
2025-01-02 19:23:41 +0900

    [refactor/#14] splash 이미지 -> 컴포넌트 구현으로 수정

--- feature/splash/src/main/java/com/qriz/app/feature/splash/SplashActivity.kt
+++ feature/splash/src/main/java/com/qriz/app/feature/splash/SplashActivity.kt
@@ -2,27 +2,17 @@ package com.qriz.app.feature.splash
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import com.qriz.app.core.designsystem.theme.QrizTheme
 import com.qriz.app.feature.main.MainActivity
 import com.qriz.app.feature.splash.model.SplashEffect
 import dagger.hilt.android.AndroidEntryPoint
-import kotlin.math.log
 
 @AndroidEntryPoint
 class SplashActivity : ComponentActivity() {
@@ -38,11 +28,11 @@ class SplashActivity : ComponentActivity() {
 
             LaunchedEffect(Unit) {
                 viewModel.effect.collect { effect ->
-                    when(effect) {
+                    when (effect) {
                         is SplashEffect.CheckLogin -> {
                             val intent =
                                 Intent(this@SplashActivity, MainActivity::class.java)
-                                .putExtra("login", effect.login)
+                                    .putExtra("login", effect.login)
                             startActivity(intent)
                             finish()
                         }
@@ -50,23 +40,9 @@ class SplashActivity : ComponentActivity() {
                 }
             }
 
-            SplashScreen()
+            QrizTheme {
+                SplashScreen()
+            }
         }
-    }
-}
-
-@Composable
-fun SplashScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .navigationBarsPadding()
-    ) {
-        Image(
-            painter = painterResource(R.drawable.img_splash_bg),
-            contentDescription = "",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize()
-        )
     }
 }
