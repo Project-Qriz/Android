8c507ce5dd788ad1ab4fa2cc52e21fe222407128
Tom <blak1002@gmail.com>
2025-01-05 19:14:54 +0900

    [refactor] Question 클래스 options 프로퍼티 Option 타입 분리
     1. 타입 안정성 + 가독성을 위해

--- core/data/onboard/onboard-api/src/main/java/com/qriz/app/core/data/onboard/onboard_api/model/Question.kt
+++ core/data/onboard/onboard-api/src/main/java/com/qriz/app/core/data/onboard/onboard_api/model/Question.kt
@@ -1,8 +1,15 @@
 package com.qriz.app.core.data.onboard.onboard_api.model
 
+/**
+ * [Test]에 속한 개별 문제
+ * @property id 문제의 고유 식별자
+ * @property question 문제의 텍스트
+ * @property options 문제에 대한 선택지[Option] 목록
+ * @property timeLimit 해당 문제의 시간 제한 (단위: 초)
+ */
 data class Question(
     val id: Long,
     val question: String,
-    val options: List<String>,
+    val options: List<Option>,
     val timeLimit: Int,
 )
--- core/data/onboard/onboard-api/src/main/java/com/qriz/app/core/data/onboard/onboard_api/model/Test.kt
+++ core/data/onboard/onboard-api/src/main/java/com/qriz/app/core/data/onboard/onboard_api/model/Test.kt
@@ -2,7 +2,7 @@ package com.qriz.app.core.data.onboard.onboard_api.model
 
 /**
  * 전체 테스트 내용을 담고 있는 모델
- * @property questions 문제 목록
+ * @property questions 문제[Question] 목록
  * @property totalTimeLimit 테스트 시간 제한
  */
 data class Test(
