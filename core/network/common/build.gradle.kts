66feafdf42155609b38776f7206360d8628b17b6
kwangyoung-swing <kwangyoung@theswing.co.kr>
2025-01-08 23:31:29 +0900

    [refactor/#9] Response 변경

--- core/network/common/build.gradle.kts
+++ core/network/common/build.gradle.kts
@@ -2,10 +2,12 @@ import com.qriz.app.setNamespace
 
 plugins {
     id("qriz.android.library.testable")
+    alias(libs.plugins.kotlinx.serialization)
 }
 android {
     setNamespace("core.network.common")
 }
 
 dependencies {
+    implementation(libs.kotlinx.serialization.json)
 }
--- core/network/user/src/main/java/com/qriz/app/core/network/user/model/response/LoginResponse.kt
+++ core/network/user/src/main/java/com/qriz/app/core/network/user/model/response/LoginResponse.kt
@@ -6,6 +6,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class LoginResponse(
     @SerialName("id") val id: Long,
-    @SerialName("userName") val userName: String,
+    @SerialName("username") val userName: String,
+    @SerialName("nickname") val nickname: String,
     @SerialName("createdAt") val createdAt: String,
 )
