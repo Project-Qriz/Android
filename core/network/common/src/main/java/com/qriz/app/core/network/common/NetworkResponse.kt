3f709866cfba536fb58fb04f6c155eb6788efdfd
KimKwangYoung <kwangyoung.dev@gmail.com>
2025-01-09 00:49:09 +0900

    [refactor/#9] Response 변경

--- core/network/common/src/main/java/com/qriz/app/core/network/common/NetworkResponse.kt
+++ core/network/common/src/main/java/com/qriz/app/core/network/common/NetworkResponse.kt
@@ -1,7 +1,11 @@
 package com.qriz.app.core.network.common
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class NetworkResponse<T>(
-    val code: Int,
-    val message: String,
-    val data: T
+    @SerialName("code") val code: Int,
+    @SerialName("msg") val message: String,
+    @SerialName("data") val data: T,
 )
