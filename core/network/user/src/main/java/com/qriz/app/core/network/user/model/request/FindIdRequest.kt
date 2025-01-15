891e1e389fd02453188c251bf6e2a4e073acbe79
KimKwangYoung <kwangyoung.dev@gmail.com>
2025-01-15 22:35:44 +0900

    [feat/#27] network api 정의

--- core/network/user/src/main/java/com/qriz/app/core/network/user/model/request/FindIdRequest.kt
+++ core/network/user/src/main/java/com/qriz/app/core/network/user/model/request/FindIdRequest.kt
@@ -5,6 +5,5 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class FindIdRequest(
-    @SerialName("nickname") val nickname: String,
     @SerialName("email") val email: String,
 )
