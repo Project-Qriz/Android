586a855869948f5b13089e41961864562cb30842
KimKwangYoung <kwangyoung.dev@gmail.com>
2025-01-19 15:32:57 +0900

    [test/#27] UserRepository 아이디 찾기 테스트코드 추가

---
+++ core/data/user/user/src/test/java/com/qriz/core/data/user/UserRepositoryTest.kt
@@ -0,0 +1,39 @@
+package com.qriz.core.data.user
+
+import com.qriz.app.core.data.user.user.repository.UserRepositoryImpl
+import com.qriz.app.core.network.common.NetworkResponse
+import com.qriz.app.core.network.user.api.UserApi
+import com.qriz.app.core.network.user.model.request.FindIdRequest
+import com.qriz.app.core.testing.MainDispatcherRule
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import org.junit.Test
+
+class UserRepositoryTest {
+
+    @get:Rule
+    val testDispatcherRule = MainDispatcherRule()
+
+    private val mockApi = mockk<UserApi>()
+    private val userRepository = UserRepositoryImpl(userApi = mockApi)
+
+    @Test
+    fun `아이디 찾기 메소드 실행 시 이메일 전송 api를 호출한다`() = runTest {
+        //given
+        val email = "test@gmail.com"
+        coEvery { mockApi.sendEmailToFindId(FindIdRequest(email = email)) } returns NetworkResponse(
+            code = 200,
+            message = "",
+            data = Unit
+        )
+
+        //when
+        userRepository.sendEmailToFindId(email)
+
+        //then
+        coVerify { mockApi.sendEmailToFindId(FindIdRequest(email = email)) }
+    }
+}
