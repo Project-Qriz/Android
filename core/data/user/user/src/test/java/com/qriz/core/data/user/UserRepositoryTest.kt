1accfe4f22bd164fc401469eea9f8daae8f97404
KimKwangYoung <kwangyoung.dev@gmail.com>
2025-01-30 15:10:27 +0900

    [test/#32] 비밀번호 찾기 기능 관련 test code 작성

--- core/data/user/user/src/test/java/com/qriz/core/data/user/UserRepositoryTest.kt
+++ core/data/user/user/src/test/java/com/qriz/core/data/user/UserRepositoryTest.kt
@@ -5,14 +5,15 @@ import com.qriz.app.core.network.common.NetworkResponse
 import com.qriz.app.core.network.common.exception.QrizNetworkException
 import com.qriz.app.core.network.user.api.UserApi
 import com.qriz.app.core.network.user.model.request.FindIdRequest
+import com.qriz.app.core.network.user.model.request.FindPwdRequest
+import com.qriz.app.core.network.user.model.request.ResetPwdRequest
+import com.qriz.app.core.network.user.model.request.VerifyPwdResetRequest
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
-import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
-import kotlin.test.fail
 
 class UserRepositoryTest {
 
@@ -39,7 +40,126 @@ class UserRepositoryTest {
     }
 
     @Test
-    fun `서버에서 받은 응답의 코드가 올바르지 않을 때 예외를 던진다`() = runTest {
+    fun `가입된 이메일로 인증번호 요청 시 비밀번호 변경을 위한 이메일 전송 api를 호출한다`() = runTest {
+        //given
+        coEvery { mockApi.sendEmailToPwd(FindPwdRequest(email = mockEmail)) } returns NetworkResponse(
+            code = 1,
+            message = "",
+            data = Unit
+        )
+
+        //when
+        userRepository.sendEmailToFindPassword(email = mockEmail)
+
+        //then
+        coVerify { mockApi.sendEmailToPwd(FindPwdRequest(email = mockEmail)) }
+    }
+
+    @Test
+    fun `가입되지 않은 이메일로 인증번호 요청 시 예외를 던진다`() = runTest {
+        //given
+        val mockFailResponse = NetworkResponse(
+            code = -1,
+            message = "가입되지 않은 이메일입니다.",
+            data = Unit,
+        )
+        coEvery {
+            mockApi.sendEmailToPwd(
+                request = FindPwdRequest(
+                    email = mockEmail
+                )
+            )
+        } returns mockFailResponse
+
+        //when
+        val exception = assertFailsWith<QrizNetworkException> {
+            userRepository.sendEmailToFindPassword(email = mockEmail)
+        }
+
+        //then
+        assert(exception.code == mockFailResponse.code)
+        assert(exception.message == mockFailResponse.message)
+    }
+
+    @Test
+    fun `비밀번호 변경을 위한 인증번호 인증 요청 시 인증 api를 호출한다`() = runTest {
+        //given
+        val mockVerifyAuthNumber = "123456"
+        coEvery {
+            mockApi.verifyPwdReset(
+                VerifyPwdResetRequest(
+                    authNumber = mockVerifyAuthNumber
+                )
+            )
+        } returns NetworkResponse(
+            code = 1,
+            message = "",
+            data = Unit,
+        )
+
+        //when
+        userRepository.verifyPasswordAuthNumber(authNumber = mockVerifyAuthNumber)
+
+        //then
+        coVerify { mockApi.verifyPwdReset(VerifyPwdResetRequest(authNumber = mockVerifyAuthNumber)) }
+    }
+
+    @Test
+    fun `잘못된 인증번호로 인증 시도 시 잘못된 응답코드를 받고 예외를 던진다`() = runTest {
+        //given
+        val mockFailVerifyAuthNumber = "111111"
+        val mockFailMessage = "잘못된 인증번호입니다."
+        val failResponse = NetworkResponse(
+            code = -1,
+            message = mockFailMessage,
+            data = Unit,
+        )
+
+        coEvery {
+            mockApi.verifyPwdReset(
+                request = VerifyPwdResetRequest(authNumber = mockFailVerifyAuthNumber)
+            )
+        } returns failResponse
+
+        //when
+        val exception = assertFailsWith<QrizNetworkException> {
+            userRepository.verifyPasswordAuthNumber(authNumber = mockFailVerifyAuthNumber)
+        }
+
+        //then
+        assert(exception.code == failResponse.code)
+        assert(exception.message == failResponse.message)
+    }
+
+    @Test
+    fun `비밀번호 변경 요청 시 비밀번호 변경 api를 호출한다`() = runTest {
+        //given
+        val mockPassword = "test1234"
+        coEvery {
+            mockApi.resetPwd(
+                request = ResetPwdRequest(
+                    password = mockPassword
+                )
+            )
+        } returns NetworkResponse(
+            code = 1,
+            message = "",
+            data = Unit,
+        )
+
+        //when
+        userRepository.resetPassword(password = mockPassword)
+
+        //then
+        coVerify {
+            mockApi.resetPwd(
+                request = ResetPwdRequest(password = mockPassword)
+            )
+        }
+    }
+
+    @Test
+    fun `아이디 찾기 이메일 요청 시 서버에서 받은 응답의 코드가 올바르지 않을 때 예외를 던진다`() = runTest {
         //given
         val failResponse = NetworkResponse(
             code = 0,
