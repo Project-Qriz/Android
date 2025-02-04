d503328a6ec9eca7f86026132743e07e6557a204
Tom <blak1002@gmail.com>
2025-02-04 20:38:16 +0900

    [refactor/#29] QrizCard elevation,border 값 수정

--- core/designsystem/src/main/java/com/qriz/app/core/designsystem/component/QrizCard.kt
+++ core/designsystem/src/main/java/com/qriz/app/core/designsystem/component/QrizCard.kt
@@ -1,26 +1,38 @@
 package com.qriz.app.core.designsystem.component
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.qriz.app.core.designsystem.theme.Gray200
+import com.qriz.app.core.designsystem.theme.White
 
 @Composable
 fun QrizCard(
     modifier: Modifier = Modifier,
-    elevation: Dp = 1.dp,
+    elevation: Dp = 3.dp,
     onClick: (() -> Unit)? = null,
+    color: Color = White,
+    border: BorderStroke? =
+        if (elevation != 0.dp) null
+        else BorderStroke(
+            width = 1.dp,
+            color = Gray200
+        ),
     content: @Composable () -> Unit,
 ) {
     Surface(
-        modifier = modifier.clickable { onClick?.invoke() },
-        shape = RoundedCornerShape(8.dp),
+        modifier = modifier,
+        onClick = onClick ?: {},
+        enabled = onClick != null,
         shadowElevation = elevation,
-        tonalElevation = 0.dp
-    ) {
-        content()
-    }
+        color = color,
+        shape = RoundedCornerShape(12.dp),
+        border = border,
+        content = content
+    )
 }
