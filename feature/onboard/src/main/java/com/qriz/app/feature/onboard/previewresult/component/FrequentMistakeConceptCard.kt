d8f907291358541eb32f52c51127a1d389d03b85
Tom <blak1002@gmail.com>
2025-02-16 19:40:30 +0900

    [feat/#29] 막대 그래프 애니메이션 추가

--- feature/onboard/src/main/java/com/qriz/app/feature/onboard/previewresult/component/FrequentMistakeConceptCard.kt
+++ feature/onboard/src/main/java/com/qriz/app/feature/onboard/previewresult/component/FrequentMistakeConceptCard.kt
@@ -1,6 +1,10 @@
 package com.qriz.app.feature.onboard.previewresult.component
 
 import android.graphics.Paint
+import android.graphics.Typeface
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -15,20 +19,27 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.RoundRect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.qriz.app.core.data.test.test_api.model.SQLDConcept
@@ -36,6 +47,7 @@ import com.qriz.app.core.designsystem.component.QrizCard
 import com.qriz.app.core.designsystem.theme.Blue100
 import com.qriz.app.core.designsystem.theme.Blue500
 import com.qriz.app.core.designsystem.theme.Gray200
+import com.qriz.app.core.designsystem.theme.Gray400
 import com.qriz.app.core.designsystem.theme.Gray500
 import com.qriz.app.core.designsystem.theme.Gray600
 import com.qriz.app.core.designsystem.theme.Gray800
@@ -47,7 +59,16 @@ import com.qriz.app.feature.onboard.previewresult.model.Ranking
 import com.qriz.app.feature.onboard.previewresult.model.WeakAreaItem
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.coroutines.launch
 import kotlin.math.min
+
+private val BAR_CHART_COLORS = listOf(
+    Blue500, Gray600, Gray400
+)
+
+fun getTestResultColor(index: Int): Color =
+    if (index > BAR_CHART_COLORS.lastIndex) BAR_CHART_COLORS.last()
+    else BAR_CHART_COLORS[index]
 
 @Composable
 fun FrequentMistakeConceptCard(
@@ -118,7 +139,6 @@ fun FrequentMistakeConceptCard(
             BarChart(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(82.dp)
                     .padding(bottom = 12.dp),
                 frequentMistakeConcepts = frequentMistakeConcepts,
             )
@@ -154,102 +174,144 @@ fun FrequentMistakeConceptCard(
     }
 }
 
+data class BarData(
+    val animation: Animatable<Float, AnimationVector1D>,
+    val targetHeight: Float,
+    val color: Color,
+    val title: String,
+    val value: Int,
+)
+
 @Composable
 private fun BarChart(
     modifier: Modifier = Modifier,
     frequentMistakeConcepts: ImmutableList<WeakAreaItem>,
+    height: Dp = 82.dp,
+    animationDurationMillis: Int = 1500,
     background: Color = White,
-    lineColor: Color = Blue100
+    axisColor: Color = Blue100,
+    axisWidth: Dp = 1.dp,
+    barGraphWidth: Dp = 28.dp,
+    barGraphCornerRadius: Dp = 8.dp,
+    spaceBetweenTextAndGraph: Dp = 10.dp,
+    barTextSize: TextUnit = 12.sp,
 ) {
-    val upperValue =
-        remember { (frequentMistakeConcepts.maxOfOrNull { it.incorrectCount }?.plus(1)) ?: 0 }
-
     val density = LocalDensity.current
-    val textPaint = remember(density) {
-        Paint().apply {
-            color = android.graphics.Color.BLACK
-            textAlign = Paint.Align.CENTER
-            textSize = with(density) { 12.sp.toPx() }
+    val upperValue = remember { (frequentMistakeConcepts.maxOfOrNull { it.incorrectCount }) ?: 0 }
+    val textHeight = remember { with(density) { 12.sp.toDp() } } //TODO : 다시 수정 필요
+    val barMaxHeight = remember {
+        with(density) { (height - (textHeight + spaceBetweenTextAndGraph)).toPx() }
+    }
+    val bars = remember(frequentMistakeConcepts) {
+        frequentMistakeConcepts.mapIndexed { index, weakAreaItem ->
+            BarData(
+                animation = Animatable(0f),
+                targetHeight = (weakAreaItem.incorrectCount.toFloat() / upperValue) * barMaxHeight,
+                color = getTestResultColor(index),
+                title = weakAreaItem.topic.title,
+                value = weakAreaItem.incorrectCount,
+            )
+        }
+    }
+
+    LaunchedEffect(bars) {
+        bars.forEach {
+            launch {
+                it.animation.animateTo(
+                    targetValue = it.targetHeight,
+                    animationSpec = tween(
+                        durationMillis = animationDurationMillis,
+                    )
+                )
+            }
         }
     }
 
     Canvas(
         modifier
+            .height(height)
             .background(background)
     ) {
-        //TODO : remember 필요 여부 확인
-        //TODO : 동점인 경우 높이가 낮은채로 같은 현상
-        //TODO : 애니메이션 추가 필요
-        //TODO : 순위별 색상이 달라야함
-        //TODO : 그래프 상단 색상 굵기 설정
-        val canvasHeight = size.height
-        val canvasWidth = size.width
-        val barGraphWidth = with(density) { 28.dp.toPx() }
-        val horizontalLineWidth = with(density) { 1.dp.toPx() }
-        val spaceBetweenTextAndGraph = with(density) { 10.dp.toPx() }
+        val canvasHeightPx = size.height
+        val canvasWidthPx = size.width
+        val barGraphWidthPx = with(density) { barGraphWidth.toPx() }
+        val axisWidthPx = with(density) { axisWidth.toPx() }
+        val spaceBetweenTextAndGraphPx = with(density) { spaceBetweenTextAndGraph.toPx() }
 
-        val maxVisibleGraphsCount = (canvasWidth / barGraphWidth).toInt()
-        val visibleGraphsCount = min(frequentMistakeConcepts.size, maxVisibleGraphsCount)
+        //TODO: 화면 크기가 큰 경우 좌우 여백이 많이 남을텐데 보여줄 수 있으면 보여주는게 맞는지 여쭤보기
+//        val maxVisibleGraphsCount = (canvasWidthPx / barGraphWidthPx).toInt()
+//        val visibleGraphsCount = min(bars.size, maxVisibleGraphsCount)
+        val visibleGraphsCount = min(bars.size, 3)
 
-        val canvasWidthExceptBarWidth = (canvasWidth - visibleGraphsCount * barGraphWidth)
+        val canvasWidthExceptBarWidthPx = (canvasWidthPx - visibleGraphsCount * barGraphWidthPx)
         val spacingFromLeft = when (visibleGraphsCount % 2) {
-            0 -> canvasWidthExceptBarWidth / (visibleGraphsCount + 1)
-            else -> canvasWidthExceptBarWidth / ((visibleGraphsCount + 1) / 2) * (5f / 17)
+            0 -> canvasWidthExceptBarWidthPx / (visibleGraphsCount + 1)
+            else -> canvasWidthExceptBarWidthPx / ((visibleGraphsCount + 1) / 2) * (5f / 17)
         }
 
-        val spacePerGraph = when (visibleGraphsCount % 2) {
-            0 -> canvasWidthExceptBarWidth / (visibleGraphsCount + 1)
-            else -> canvasWidthExceptBarWidth / ((visibleGraphsCount + 1) / 2) * (12f / 17)
+        val spacePerGraphPx = when (visibleGraphsCount % 2) {
+            0 -> canvasWidthExceptBarWidthPx / (visibleGraphsCount + 1)
+            else -> canvasWidthExceptBarWidthPx / ((visibleGraphsCount + 1) / 2) * (12f / 17)
         }
 
         //HorizontalLine
         drawLine(
             start = Offset(
                 x = 0F,
-                y = canvasHeight
+                y = canvasHeightPx
             ),
             end = Offset(
-                x = canvasWidth,
-                y = canvasHeight
+                x = canvasWidthPx,
+                y = canvasHeightPx
             ),
-            color = lineColor,
-            strokeWidth = horizontalLineWidth
+            color = axisColor,
+            strokeWidth = axisWidthPx
         )
-        val textHeight = with(density) { 12.sp.toPx() } //TODO : 다시 수정 필요
-        val barMaxHeight = canvasHeight - (textHeight + spaceBetweenTextAndGraph)
 
         //BarGraph
-        frequentMistakeConcepts.take(visibleGraphsCount)
-            .forEachIndexed { index, frequentMistakeConcept ->
-                val barStartX = spacingFromLeft + (barGraphWidth * index) + (spacePerGraph * index)
-                val barStartY =
-                    textHeight + spaceBetweenTextAndGraph + ((upperValue - frequentMistakeConcept.incorrectCount).toFloat() / upperValue) * barMaxHeight
+        bars.take(visibleGraphsCount)
+            .forEachIndexed { index, barData ->
+                val barStartX =
+                    spacingFromLeft + (barGraphWidthPx * index) + (spacePerGraphPx * index)
+                val cornerRadiusPx = with(density) { barGraphCornerRadius.toPx() }
+                val cornerRadius = CornerRadius(cornerRadiusPx, cornerRadiusPx)
+                val path = Path().apply {
+                    addRoundRect(
+                        RoundRect(
+                            rect = Rect(
+                                offset = Offset(
+                                    x = barStartX,
+                                    y = canvasHeightPx
+                                ),
+                                size = Size(
+                                    width = barGraphWidthPx,
+                                    height = -barData.animation.value
+                                )
+                            ),
+                            topLeft = cornerRadius,
+                            topRight = cornerRadius,
+                        )
+                    )
+                }
+                drawPath(path, color = barData.color)
 
-                drawRoundRect(
-                    color = Blue500,
-                    topLeft = Offset(
-                        x = barStartX,
-                        y = barStartY
-                    ),
-                    size = Size(
-                        width = barGraphWidth,
-                        height = (frequentMistakeConcept.incorrectCount.toFloat() / upperValue) * barMaxHeight
-                        //((value / upperValue) * canvasHeight) - spacingFromBottom
-                    ),
-                    cornerRadius = CornerRadius(10f, 10f)
-                )
-
+                //BarGraph Top Text
                 drawContext.canvas.nativeCanvas.apply {
-                    var displayText = frequentMistakeConcept.topic.title
+                    var displayText = barData.title
                     if (displayText.length > 3) {
                         displayText = displayText.take(3) + "..."
                     }
 
                     drawText(
                         displayText,
-                        barStartX + (barGraphWidth / 2),
-                        barStartY - spaceBetweenTextAndGraph,
-                        textPaint
+                        barStartX + (barGraphWidthPx / 2),
+                        canvasHeightPx - barData.animation.value - spaceBetweenTextAndGraphPx,
+                        Paint().apply {
+                            color = getTestResultColor(index).toArgb()
+                            textAlign = Paint.Align.CENTER
+                            textSize = with(density) { barTextSize.toPx() }
+                            typeface = Typeface.create(Typeface.DEFAULT, Typeface.BOLD)
+                        }
                     )
                 }
             }
@@ -312,7 +374,7 @@ private fun WrongQuestionRankingItem(
                     text = rankingText,
                     color = if (isHeaderRankingItem) rankingColor else Color.Transparent,
                     style = QrizTheme.typography.caption.copy(
-                        fontWeight = FontWeight.Bold
+                        fontWeight = FontWeight.Bold,
                     )
                 )
             }
@@ -381,11 +443,6 @@ private fun FrequentMistakeConceptCardPreview() {
                     topic = SQLDConcept.UNDERSTANDING_TRANSACTIONS,
                     incorrectCount = 1
                 ),
-//                WeakAreaItem(
-//                    ranking = Ranking.THIRD,
-//                    topic = SQLDConcept.UNDERSTANDING_TRANSACTIONS,
-//                    incorrectCount = 1
-//                ),
             )
         )
     }
