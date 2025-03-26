a6cb98b3c5350c08352d09fd24820456d55cfdf3
KimKwangYoung <kwangyoung.dev@gmail.com>
2025-03-26 22:44:48 +0900

    [feature/#41] Repository 카테고리 및 개념서 가져오기 기능 추가

--- core/data/conceptbook/conceptbook/src/main/java/com/qriz/app/core/data/conceptbook/conceptbook/repository/ConceptBookRepositoryImpl.kt
+++ core/data/conceptbook/conceptbook/src/main/java/com/qriz/app/core/data/conceptbook/conceptbook/repository/ConceptBookRepositoryImpl.kt
@@ -1,7 +1,11 @@
 package com.qriz.app.core.data.conceptbook.conceptbook.repository
 
 import com.qriz.app.core.data.conceptbook.conceptbook.data.SubjectData
+import com.qriz.app.core.data.conceptbook.conceptbook.mapper.toCategory
+import com.qriz.app.core.data.conceptbook.conceptbook.mapper.toConceptBook
 import com.qriz.app.core.data.conceptbook.conceptbook.mapper.toSubject
+import com.qriz.app.core.data.conceptbook.conceptbook_api.model.Category
+import com.qriz.app.core.data.conceptbook.conceptbook_api.model.ConceptBook
 import com.qriz.app.core.data.conceptbook.conceptbook_api.model.Subject
 import com.qriz.app.core.data.conceptbook.conceptbook_api.repository.ConceptBookRepository
 import javax.inject.Inject
@@ -9,5 +13,29 @@ import javax.inject.Inject
 class ConceptBookRepositoryImpl @Inject constructor() : ConceptBookRepository {
     override suspend fun getData(): List<Subject> {
         return SubjectData.entries.map { it.toSubject() }
+    }
+
+    override suspend fun getCategoryData(categoryName: String): Category {
+        for (subject in SubjectData.entries) {
+            val result = subject.categories.find { it.categoryName == categoryName }
+            if (result != null) {
+                return result.toCategory()
+            }
+        }
+
+        throw IllegalArgumentException("존재하지 않는 카테고리입니다.")
+    }
+
+    override suspend fun getConceptBook(id: Long): ConceptBook {
+        for (subject in SubjectData.entries) {
+            for (category in subject.categories) {
+                val result = category.concept.find { it.id == id }
+                if (result != null) {
+                    return result.toConceptBook()
+                }
+            }
+        }
+
+        throw IllegalArgumentException("존재하지 않는 개념서입니다.")
     }
 }
