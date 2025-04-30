65a003b32e34d1063bf0dd0c77c35561ec988eef
KimKwangYoung <kwangyoung.dev@gmail.com>
2025-04-30 23:18:13 +0900

    [feat/#45] model 모듈 추가 및 api 응답 wrapper 클래스 정의

---
+++ core/model/src/main/java/com/qriz/app/core/model/ApiResult.kt
@@ -0,0 +1,13 @@
+package com.qriz.app.core.model
+
+import java.io.IOException
+
+sealed interface ApiResult<out T> {
+    data class Success<T>(val data: T): ApiResult<T>
+
+    data class Failure(val code: Int, val message: String): ApiResult<Nothing>
+
+    data class NetworkError(val exception: IOException): ApiResult<Nothing>
+
+    data class UnknownError(val throwable: Throwable?): ApiResult<Nothing>
+}
