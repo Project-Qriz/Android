c14fda591ae9d95d0db48f2df6d6d46ef3479c78
KimKwangYoung <kwangyoung.dev@gmail.com>
2025-05-03 16:26:52 +0900

    [refactor/#45] 로그인 및 회원가입 관련 api 응답 형태 서버에 맞게 변경

--- core/network/user/src/main/java/com/qriz/app/core/network/user/model/response/JoinResponse.kt
+++ core/network/user/src/main/java/com/qriz/app/core/network/user/model/response/JoinResponse.kt
@@ -6,7 +6,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class JoinResponse(
     @SerialName("id") val id: Long,
-    @SerialName("username") val username: String,
-    @SerialName("name") val name: String,
-    @SerialName("date") val date: String,
+    @SerialName("username") val userId: String,
+    @SerialName("nickname") val name: String,
 )
--- core/network/user/src/main/java/com/qriz/app/core/network/user/model/response/LoginResponse.kt
+++ core/network/user/src/main/java/com/qriz/app/core/network/user/model/response/LoginResponse.kt
@@ -4,16 +4,9 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 /** 서버측 요구 사항에 맞춰진 필드명
- * @property id         유저 식별 ID
- * @property userName   사용자 ID
- * @property nickname   유저 성명
- * @property createdAt  가입일
+ * @property name 유저 성명
  * */
 @Serializable
 data class LoginResponse(
-    @SerialName("id") val id: Long,
-    @SerialName("username") val userName: String,
-    @SerialName("nickname") val nickname: String,
-    @SerialName("createdAt") val createdAt: String,
-    @SerialName("previewTestStatus") val previewTestStatus: String,
+    @SerialName("name") val userName: String,
 )
