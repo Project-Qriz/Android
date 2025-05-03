b0d38ca0470605bc25fc19c63beea0e28f6be7af
KimKwangYoung <kwangyoung.dev@gmail.com>
2025-05-03 16:43:07 +0900

    [ui/#45] 공통 메세지 상수 선언

---
+++ core/ui/common/src/main/java/com/qriz/app/core/ui/common/resource/String.kt
@@ -0,0 +1,9 @@
+package com.qriz.app.core.ui.common.resource
+
+const val NETWORK_IS_UNSTABLE = "인터넷 연결이 불안정해요."
+const val CHECK_NETWORK_AND_TRY_AGAIN = "Wi-Fi나 셀룰러 데이터 연결 상태를 확인하고 다시 시도해주세요."
+
+const val SERVER_ERROR_OCCURRED = "서버 오류가 발생했어요"
+const val CONTACT_DEVELOPER_IF_PERSISTS = "증상이 지속된다면 개발자에게 문의해주세요"
+
+const val UNKNOWN_ERROR = "알 수 없는 오류가 발생했습니다."
