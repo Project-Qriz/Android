bb0df787915f75a3a548b58179547014acd8f8b9
KimKwangYoung <kwangyoung.dev@gmail.com>
2025-05-06 16:25:07 +0900

    [refactor/#45] 비밀번호 인증번호 확인 API body 필드 추가

--- core/network/user/src/main/java/com/qriz/app/core/network/user/model/request/VerifyPwdResetRequest.kt
+++ core/network/user/src/main/java/com/qriz/app/core/network/user/model/request/VerifyPwdResetRequest.kt
@@ -5,5 +5,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class VerifyPwdResetRequest(
+    @SerialName("email") val email: String,
     @SerialName("authNumber") val authNumber: String,
 )
