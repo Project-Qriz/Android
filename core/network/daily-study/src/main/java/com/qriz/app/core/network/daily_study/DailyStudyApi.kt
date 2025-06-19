58bca1e0f79843d884bec7acdf091942224f7b6f
KimKwangYoung <kwangyoung.dev@gmail.com>
2025-06-19 22:22:44 +0900

    [feat/#48] API 응답 변경

--- core/network/daily-study/src/main/java/com/qriz/app/core/network/daily_study/DailyStudyApi.kt
+++ core/network/daily-study/src/main/java/com/qriz/app/core/network/daily_study/DailyStudyApi.kt
@@ -1,13 +1,14 @@
 package com.qriz.app.core.network.daily_study
 
+import com.qriz.app.core.model.ApiResult
 import com.qriz.app.core.network.daily_study.model.response.DailyStudyPlanResponse
 import com.qriz.app.core.network.daily_study.model.response.WeeklyRecommendationResponseContainer
 import retrofit2.http.GET
 
 interface DailyStudyApi {
     @GET("/api/v1/daily/plan")
-    suspend fun getDailyStudyPlan(): List<DailyStudyPlanResponse>
+    suspend fun getDailyStudyPlan(): ApiResult<List<DailyStudyPlanResponse>>
 
     @GET("/api/v1/recommend/weekly")
-    suspend fun getWeeklyRecommendation(): WeeklyRecommendationResponseContainer
+    suspend fun getWeeklyRecommendation(): ApiResult<WeeklyRecommendationResponseContainer>
 }
