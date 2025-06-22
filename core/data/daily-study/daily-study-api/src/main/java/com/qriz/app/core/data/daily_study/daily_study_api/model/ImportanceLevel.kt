dde43d151fe8cff9daf0a64a3b57f60033c415c3
KimKwangYoung <kwangyoung.dev@gmail.com>
2025-06-22 16:26:08 +0900

    [feat/#48] ImporanceLevel enum 값 추가

--- core/data/daily-study/daily-study-api/src/main/java/com/qriz/app/core/data/daily_study/daily_study_api/model/ImportanceLevel.kt
+++ core/data/daily-study/daily-study-api/src/main/java/com/qriz/app/core/data/daily_study/daily_study_api/model/ImportanceLevel.kt
@@ -2,6 +2,7 @@ package com.qriz.app.core.data.daily_study.daily_study_api.model
 
 enum class ImportanceLevel(val displayName: String) {
     HIGH("상"),
+    MEDIUM("중"),
     LOW("하");
 
     companion object {
