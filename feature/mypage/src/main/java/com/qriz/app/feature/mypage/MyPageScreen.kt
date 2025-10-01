d947a105ae4e2e126fc704e5cf3d2c3b86f723a6
KimKwangYoung <kwangyoung.dev@gmail.com>
2025-10-02 00:07:08 +0900

    [feat/#62] 마이페이지 UI 구현

--- feature/mypage/src/main/java/com/qriz/app/feature/mypage/MyPageScreen.kt
+++ feature/mypage/src/main/java/com/qriz/app/feature/mypage/MyPageScreen.kt
@@ -1,7 +1,10 @@
 package com.qriz.app.feature.mypage
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -10,15 +13,21 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.qriz.app.core.designsystem.component.NavigationType
+import com.qriz.app.core.designsystem.component.QrizTopBar
+import com.qriz.app.core.designsystem.theme.Blue50
 import com.qriz.app.core.designsystem.theme.QrizTheme
 import com.qriz.app.feature.base.extention.collectSideEffect
+import com.qriz.app.feature.mypage.component.CustomerServiceSection
+import com.qriz.app.feature.mypage.component.FeatureSection
+import com.qriz.app.feature.mypage.component.UserSection
 
 
 @Composable
 fun MyPageScreen(
-    viewModel: MyPageViewModel = hiltViewModel(),
-    onShowSnackBar: (String) -> Unit
+    viewModel: MyPageViewModel = hiltViewModel(), onShowSnackBar: (String) -> Unit
 ) {
     val uiState by viewModel.uiState.collectAsState()
     val context = LocalContext.current
@@ -36,19 +45,51 @@ fun MyPageScreen(
 
 @Composable
 fun MyPageContent() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = Blue50,
+            )
     ) {
-        Text(
-            text = "MyPageScreen",
-            style = QrizTheme.typography.display1
+        QrizTopBar(
+            navigationType = NavigationType.NONE,
+            title = "마이페이지",
+            onNavigationClick = {},
+        )
+
+        UserSection(
+            modifier = Modifier.padding(
+                vertical = 24.dp,
+                horizontal = 18.dp,
+            ),
+            userName = "홍길동",
+            onClick = {},
+        )
+
+        FeatureSection(
+            modifier = Modifier.padding(horizontal = 18.dp),
+            onClickReset = {},
+            onClickRegisterExam = {},
+        )
+
+        CustomerServiceSection(
+            modifier = Modifier.padding(
+                horizontal = 18.dp,
+                vertical = 32.dp,
+            ),
+            onClickVersion = {},
+            onClickServiceTerms = {},
+            onClickPrivacyPolicy = {},
         )
     }
 }
 
+
 @Preview(showBackground = true)
 @Composable
 fun MyPageContentPreview() {
-
+    QrizTheme {
+        MyPageContent()
+    }
 }
---
+++ feature/mypage/src/main/java/com/qriz/app/feature/mypage/component/FeatureSection.kt
@@ -0,0 +1,92 @@
+package com.qriz.app.feature.mypage.component
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.qriz.app.core.designsystem.component.QrizCard
+import com.qriz.app.core.designsystem.theme.Gray800
+import com.qriz.app.core.designsystem.theme.QrizTheme
+import com.qriz.app.feature.mypage.R
+
+@Composable
+internal fun FeatureSection(
+    modifier: Modifier = Modifier,
+    onClickReset: () -> Unit,
+    onClickRegisterExam: () -> Unit,
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        FeatureItem(
+            modifier = Modifier.weight(1f),
+            title = stringResource(R.string.my_plan_reset),
+            iconRes = R.drawable.ic_reset,
+            onClick = onClickReset,
+        )
+        FeatureItem(
+            modifier = Modifier.weight(1f),
+            title = stringResource(R.string.my_exam_register),
+            iconRes = R.drawable.ic_exam_register,
+            onClick = onClickReset,
+        )
+    }
+}
+
+@Composable
+private fun FeatureItem(
+    modifier: Modifier = Modifier,
+    @DrawableRes iconRes: Int,
+    title: String,
+    onClick: () -> Unit,
+) {
+    QrizCard(
+        modifier = modifier.clickable(onClick = onClick),
+        cornerRadius = 16.dp,
+    ) {
+        Column(
+            modifier = Modifier.padding(vertical = 12.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                modifier = Modifier.size(32.dp),
+                imageVector = ImageVector.vectorResource(iconRes),
+                tint = Color.Unspecified,
+                contentDescription = null,
+            )
+
+            Text(
+                text = title,
+                style = QrizTheme.typography.body1.copy(color = Gray800)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun FeatureSectionPreview() {
+    QrizTheme {
+        FeatureSection(
+            onClickReset = {},
+            onClickRegisterExam = {},
+        )
+    }
+}
